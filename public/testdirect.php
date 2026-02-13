04873ca5f7de15cb9942cc194fd5fff2797a7eb4
haziqfaruqi” <haziqfaruqi@gmail.com>
2026-02-13 22:19:20 +0800

    Use Railway public URL proxy (tramway.proxy.rlwy.net:26362)

--- public/testdirect.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Test direct environment access
-echo "MYSQLHOST: " . getenv('MYSQLHOST') . "\n";
-echo "MYSQLDATABASE: " . getenv('MYSQLDATABASE') . "\n";
-echo "MYSQLPORT: " . getenv('MYSQLPORT') . "\n";
-echo "MYSQLUSER: " . getenv('MYSQLUSER') . "\n";
-echo "MYSQLPASSWORD: " . getenv('MYSQLPASSWORD') . "\n";
